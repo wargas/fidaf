98cd842409319e493df437ae20626cb6287e5a4c
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-01-13 14:13:02 -0300

    add-rotues

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,33 @@ import app from "./app";
 import { database } from "./database";
 
 app.get('/cron', (_, res) => {
+   
     res.send({nextDate: cron.nextDate()})
 })
 
 app.get('/resumo', async(_, res) => {
     const data = await database.table('resumo')
+        .orderBy('mes', 'desc')
+
+    return data;
+})
+
+app.get('/correcao', async(_, res) => {
+    const data = await database.table('correcao')
+        .orderBy('mes', 'desc')
+
+    return data;
+})
+
+app.get('/receitas', async(_, res) => {
+    const data = await database.table('receitas')
+        .orderBy('mes', 'desc')
+
+    return data;
+})
+
+app.get('/ipca', async(_, res) => {
+    const data = await database.table('ipca')
         .orderBy('mes', 'desc')
 
     return data;
