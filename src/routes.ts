189281d2dfa0c68ef2c86e3590439980a3da1c5d
wargas <teixeira.wargas@gmail.com>
2025-02-12 08:44:03 -0300

    api:calculo

--- src/routes.ts
+++ src/routes.ts
@@ -1,66 +1,145 @@
+import { format } from "date-fns";
+import { sumBy } from "lodash";
 import { cron } from "../cron";
 import app from "./app";
 import { database } from "./database";
 
 app.get('/cron', (_, res) => {
-   
-    res.send({nextDate: cron.nextDate()})
+
+    res.send({ nextDate: cron.nextDate() })
 })
 
-app.get('/resumo', async(_, res) => {
+app.get('/resumo', async (_, res) => {
     const data = await database.table('resumo')
         .orderBy('mes', 'desc')
 
     return data;
 })
 
-app.get('/correcao', async(_, res) => {
+app.get('/correcao', async (_, res) => {
     const data = await database.table('correcao')
         .orderBy('mes', 'desc')
 
     return data;
 })
 
-app.get('/receitas', async(_, res) => {
+app.get('/receitas', async (_, res) => {
     const data = await database.table('receitas')
         .orderBy('mes', 'desc')
 
     return data;
 })
 
-app.get('/recolhimentos', async(req, res) => {
+app.get('/recolhimentos', async (req, res) => {
 
-    const { inicio = '', fim = '', orderBy = 'data'} = req.query as any;
+    const { inicio = '', fim = '', orderBy = 'data' } = req.query as any;
 
     const query = database.table('recolhimento_diario')
 
-    if(String(orderBy).startsWith('-')) {
+    if (String(orderBy).startsWith('-')) {
         query.orderBy(String(orderBy).replace('-', ''), 'DESC')
     } else {
         query.orderBy(String(orderBy).replace('-', ''), 'ASC')
     }
 
-    if(inicio) {
+    if (inicio) {
         query.where('data', '>=', inicio)
     }
 
-    if(fim) {
+    if (fim) {
         query.where('data', '<=', fim)
     }
 
 
     const data = await query;
-        // .orderBy('data', 'desc')
+    // .orderBy('data', 'desc')
 
-    return data.map(d => ({...d, 
+    return data.map(d => ({
+        ...d,
         nominal: parseFloat(d.nominal),
         corrigido: parseFloat(d.corrigido),
     }));
 })
 
-app.get('/ipca', async(_, res) => {
+app.get('/calculo', async (req, res) => {
+    const defaultEnd = new Date()
+    const { inicio = '2025-01-01', fim = format(defaultEnd, 'Y-MM-dd') } = req.query as any;
+
+    const inicioPrev = String(inicio).replace('2025', '2024')
+    const fimPrev = String(fim).replace('2025', '2024')
+
+    
+    
+    const queryPrev = (await database.table('recolhimento_diario')
+        .whereBetween('data', [inicioPrev, fimPrev])).map(r => ({...r,
+            nominal: parseFloat(r.nominal),
+            corrigido: parseFloat(r.corrigido)
+        }))
+    
+    const queryCurr = (await database.table('recolhimento_diario')
+        .whereBetween('data', [inicio, fim])).map(r => ({...r,
+            nominal: parseFloat(r.nominal),
+            corrigido: parseFloat(r.corrigido)
+        }))
+
+    const receitas = {
+        prev: {
+            nominal: 0,
+            corrigida: 0
+        },
+        curr: {
+            nominal: 0,
+            corrigida: 0
+        },
+        incremento: {
+            valor: 0,
+            porcentagem: 0
+        },
+        premio: {
+            porcentagem: 0,
+            valor: 0
+        }, 
+        distribuicao: {
+            pontos: 513.9,
+            auditor: 0,
+            analista: 0
+        }
+
+    }
+
+    receitas.prev.nominal = sumBy(queryPrev, 'nominal')
+    receitas.prev.corrigida = sumBy(queryPrev, 'corrigido')
+    receitas.curr.nominal = sumBy(queryCurr, 'nominal')
+    receitas.curr.corrigida = sumBy(queryCurr, 'corrigido')
+    receitas.incremento.valor = receitas.curr.corrigida - receitas.prev.corrigida
+
+    receitas.incremento.valor = receitas.incremento.valor < 0 ? 0 : receitas.incremento.valor
+    receitas.incremento.porcentagem = receitas.incremento.valor / receitas.prev.corrigida
+
+    receitas.premio.porcentagem = 0.1
+
+    if(receitas.incremento.porcentagem >= 0.1) {
+        receitas.premio.porcentagem = 0.15
+    }
+
+    if(receitas.incremento.porcentagem >= 0.06 && receitas.incremento.porcentagem < 0.1) {
+        receitas.premio.porcentagem = 0.125
+    }
+
+    receitas.premio.valor = receitas.premio.porcentagem * receitas.incremento.valor * 0.8
+
+    receitas.distribuicao.auditor = receitas.premio.valor / receitas.distribuicao.pontos * 3
+    receitas.distribuicao.analista = receitas.premio.valor / receitas.distribuicao.pontos * 2.1
+
+    res.send(receitas)
+
+    
+});
+
+app.get('/ipca', async (_, res) => {
     const data = await database.table('ipca')
         .orderBy('mes', 'desc')
 
     return data;
-})
+});
+
