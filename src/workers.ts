225caa5f335edd73e5c6dcb7d3d9424d559c4883
wargas <teixeira.wargas@gmail.com>
2025-03-09 07:54:17 -0300

    bull:error

--- src/workers.ts
+++ src/workers.ts
@@ -4,26 +4,30 @@ import { carregarDia } from "./carregar_dia";
 import { database } from "./database";
 import { connection } from "./redis";
 
-export const workerLoadDays = new Worker<number, any, string>('loadDays', async Job => {
-    console.log('Iniciando...')
+const workerLoadDays = new Worker<number, any, string>('loadDays', async job => {
+    job.updateProgress({state: 'obtendo subalineas'})
     const subalineas = (await database.table('codigos')
         .groupBy('subalinea')
         .select('subalinea'))
         .map(c => c.subalinea)
 
+    job.updateProgress({state: 'obtendo codigos'})
     const codigos = (await database.table('codigos'))
         .map(c => c.codigo)
 
 
     const lastDay = new Date()
-    let currentDay = subDays(new Date(), Job.data)
+    let currentDay = subDays(new Date(), job.data)
 
+    job.updateProgress({state: 'iniciando o processo'})
     while (isBefore(currentDay, lastDay)) {
         currentDay = addDays(currentDay, 1)
 
         const formated = format(currentDay, 'y-MM-dd')
 
         for await (let subalinea of subalineas) {
+
+            job.updateProgress({state: `${formated}:${subalinea}`})
             const data = await carregarDia(formated, subalinea);
 
             if (data.length > 0) {
@@ -33,9 +37,21 @@ export const workerLoadDays = new Worker<number, any, string>('loadDays', async 
                     .merge()
             }
 
+            
         }
-        console.log(formated)
     }
+
+    job.updateProgress({state: 'concluido'})
 
     return true;
 }, { connection, autorun: false })
+
+workerLoadDays.on('error', () => {
+    console.log('ocorreu um erro')
+})
+
+workerLoadDays.on('progress', (job, progress) => {
+    console.log(job.name, progress)
+})
+
+export { workerLoadDays };
