b3d79cc692a42fcb2eaf1eeda28dd68e26f7e67d
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-11-20 14:04:46 -0300

    try

--- src/workers/workerLoadDay.ts
+++ src/workers/workerLoadDay.ts
@@ -15,21 +15,25 @@ export const queueLoadDay = new Queue<QueueInput>(queueName, {
 
 export const wokerLoadDay = new Worker<QueueInput>(queueName, async job => {
 
-    
-    job.log('carregando dia')
-    const data = await carregarDia(job.data.day, job.data.subalinea);
 
-    job.log('atualizando data')
-    console.log(job.data.day, job.data.subalinea)
-    if (data.length > 0) {
-        await database.table('recolhimento')
-            .insert(data.filter(d => job.data.codigos.includes(d.codigo!)))
-            .onConflict()
-            .merge()
+    try {
+        job.log('carregando dia')
+        const data = await carregarDia(job.data.day, job.data.subalinea);
 
-        return data;
+        job.log('atualizando data')
+        console.log(job.data.day, job.data.subalinea)
+        if (data.length > 0) {
+            await database.table('recolhimento')
+                .insert(data.filter(d => job.data.codigos.includes(d.codigo!)))
+                .onConflict()
+                .merge()
+
+            return data;
+        }
+
+    } catch (error) {
+        throw new Error("Erro ao obter dados")
     }
 
-
     return [];
-}, {connection, autorun: false})
+}, { connection, autorun: false })
